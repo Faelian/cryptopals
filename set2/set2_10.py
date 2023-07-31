cefd7c0824bd26361be49b39e2d70bb4731373f8
Olivier Lasne <lasneolivier@gmail.com>
2023-07-31 15:02:27 +0200

    fixed PKCS#7 and challenge 10

--- set2/set2_10.py
+++ set2/set2_10.py
@@ -4,15 +4,24 @@
 from Crypto.Cipher import AES
 import base64
 
+
 def pad_pkcs7(unpadded_block, length):
 	bytes_to_add = length - len(unpadded_block)
-	padded_block = unpadded_block + b'\x04' * bytes_to_add
+	padded_block = unpadded_block + chr(bytes_to_add).encode() * bytes_to_add
 
 	return padded_block
 
+# This function fail if we have incorrect padding
 def unpad_pkcs7(padded_block):
-	while (padded_block[-1] == 0x04):
-		padded_block = padded_block[:-1]
+	pad_length = padded_block[-1]
+
+	# let check if we effectively have padding
+	padding = padded_block[-pad_length:]
+	for byte_pad in padding:
+		if byte_pad != pad_length:
+			raise ValueError(f'PKCS#7 padding error: every byte of the padding should have the value of the length of the padding.')
+
+	padded_block = padded_block[:-pad_length]
 
 	return padded_block
 
@@ -21,12 +30,23 @@ def xor_blocks(block1, block2):
 	return bytes(xored_array)
 
 def aes_cbc_encrypt(iv, cleartext, key):
-	
+
 	# divide cleartext in blocks (AES block = 16 bytes)
 	blocks = [cleartext[i:i+16] for i in range(0, len(cleartext), 16)]
 	
-	# pad the last block of cleartext with PKCS7
-	blocks[-1] = pad_pkcs7(blocks[-1], 16)
+	# AES-CBC must always be padded
+	# If the message length is a multiple of 16, we add a new block made of padding.
+
+	# Without this, the decryption code will assume that the end of the message
+	# is padded, and remove part of the message. (In PKCS#7, )
+
+	# Add a block made of PKCS#7 padding
+	if len(cleartext) % 16 == 0:
+		blocks.append(pad_pkcs7(b'', 16))
+
+	# pad the last block of cleartext with PKCS#7
+	else:
+		blocks[-1] = pad_pkcs7(blocks[-1], 16)
 
 	
 	# for the exercice we implement CBC with ECB
@@ -77,9 +97,11 @@ def aes_cbc_decrypt(iv, ciphertext, key):
 	return cleartext
 
 if __name__ == '__main__':
+	IV = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+	KEY = b'YELLOW SUBMARINE'
+
 	with open ('10.txt', 'r') as f:
-		iv = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 		ciphertext = base64.b64decode(f.read())
 	
-		cleartext = aes_cbc_decrypt(iv, ciphertext, 'YELLOW SUBMARINE').decode()
+		cleartext = aes_cbc_decrypt(IV, ciphertext, KEY).decode()
 		print (cleartext)
