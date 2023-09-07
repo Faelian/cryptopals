8bde90d4e76a07d730c3e09e58260aece49e88c0
olivier lasne <lasneolivier@gmail.com>
2023-09-07 16:41:28 +0200

    challenge 19: split in single key xor ciphertexts

--- set3/set3_19.py
+++ set3/set3_19.py
@@ -4,8 +4,10 @@
 from Crypto.Cipher import AES
 from hexdoor import hexdump
 from base64 import b64decode
+import string
 
 from set3_18 import aes_ctr, generate_keystream, xor_blocks
+from pprint import pprint
 
 BLOCKSIZE = 16
 IV = b'\x00'*8
@@ -54,14 +56,118 @@ SECRET_MESSAGES = [
 	'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4='
 ]
 
+# Copy pasted from  set1_3.py
 
-ciphertexts = []
+# https://crypto.stackexchange.com/questions/30209/developing-algorithm-for-detecting-plain-text-via-frequency-analysis
+# on donne un score à chaque string (est-ce un texte anglais ?) 
+def scoreString (dechiphered_string) :
+	expFreqsIncludingSpace = [
+		0.0651738, 0.0124248, 0.0217339, 0.0349835,  #'A', 'B', 'C', 'D',...
+		0.1041442, 0.0197881, 0.0158610, 0.0492888, 
+		0.0558094, 0.0009033, 0.0050529, 0.0331490,
+		0.0202124, 0.0564513, 0.0596302, 0.0137645, 
+		0.0008606, 0.0497563, 0.0515760, 0.0729357, 
+		0.0225134, 0.0082903, 0.0171272, 0.0013692, 
+		0.0145984, 0.0007836, 0.1918182] ; #'Y', 'Z', ' '
 
-for message in SECRET_MESSAGES:
-	encrypted_message = aes_ctr(IV, b64decode(message), KEY)
-	ciphertexts.append(encrypted_message)
+
+	dechiphered_string = dechiphered_string.upper()
+	CHARS_CONSIDERED = 27 # lettres en majuscules + espace
+
+	char_count = [0] * CHARS_CONSIDERED
+	char_freq  = [None] * CHARS_CONSIDERED
+
+	# des fois qu'on ait de caractères bizarres au milieu qui ne comptent pas ?
+	totCount = 0
+
+	for c in dechiphered_string :
+		index = ord(c) - ord('A')
+
+		if (index >= 0 and index <26):
+			char_count[index] = char_count[index] + 1
+			totCount = totCount + 1
+
+		elif (c == ' '):
+			char_count[26] = char_count[26] + 1
+			totCount = totCount + 1
+
+		# on met un score elevé si on a des caractères non ascii
+		elif (c not in string.printable):
+			return 100000
 
 
-for encrypted_message in ciphertexts:
-	print(encrypted_message.hex())
-	print(len(encrypted_message))
+	if totCount == 0 : totCount = 1
+	
+	chiSquareScore = 0
+
+	for i in range (0, len(char_count)):
+		char_freq[i] = char_count[i] / totCount
+		chiSquareScore = chiSquareScore + ((char_freq[i] - expFreqsIncludingSpace[i]) * (char_freq[i] - expFreqsIncludingSpace[i]) / \
+		expFreqsIncludingSpace[i]) 
+
+	return chiSquareScore
+
+def break_single_key_xor(encrypted_bytes):
+	final_score = 100000000000
+	final_key = None
+	message = None
+
+	# déchiffrement avec tous les clés possibles
+	for key in range (0x00, 0xff + 1):
+		decrypted = [key ^ a for a in encrypted_bytes] 
+
+		# si on ne peut pas décoder la chaine
+		try:
+			decrypted_string = bytes(decrypted).decode('utf-8')
+		
+		except UnicodeDecodeError as e:
+			#print (repr(e))
+			continue
+
+		score = scoreString (decrypted_string)
+
+		# si on a plusieurs message avec le même score, on les affiche tous	
+		if (score < final_score):
+			message = decrypted_string
+			final_key = key
+			final_score = score
+
+	return final_key, message
+
+
+# Functions for this set
+
+
+def split_in_single_key(message, single_key_blocks):
+	for i in range(len(message)):
+		single_key_blocks[i].append(message[i])
+
+if __name__ == '__main__':
+	# initalize the challenge:
+	# encrypt every message with the KEY
+	ciphertexts = []
+
+	for message in SECRET_MESSAGES:
+		encrypted_message = aes_ctr(IV, b64decode(message), KEY)
+		ciphertexts.append(encrypted_message)
+
+	# Since the same keystream is use to encrypt every message,
+	# we can group bytes of ciphertext by the keystream that encrypt the message
+
+	max_length_of_ciphertext = max([len(ciphertext) for ciphertext in ciphertexts])
+
+	single_key_blocks = [[] for _ in range(max_length_of_ciphertext)]
+
+
+	# create array of single byte arry
+	for encrypted_message in ciphertexts:
+		split_in_single_key(encrypted_message, single_key_blocks)	
+
+	pprint(single_key_blocks[0])	
+
+
+	for single_key_bytes in single_key_blocks:
+		print(bytes(single_key_bytes).hex())
+
+	key, message = break_single_key_xor(single_key_blocks[0])
+	print(message)
