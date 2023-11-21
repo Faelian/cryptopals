0a203268a0d112eafbfb2647aeaf87ca5053a3f5
Lasne Olivier <lasneolivier@gmail.com>
2023-11-21 14:30:02 +0100

    advance MT19937, but the results are incorrect

---
+++ set3/MT19937.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+w, n, m, r = 32, 624, 397, 31
+
+
+
+print("Hello!")
--- set3/set3_21.py
+++ set3/set3_21.py
@@ -14,12 +14,12 @@ t, c = 15, 0xefc60000
 l = 18
 f = 1812433253
 
-print(f"w:{w}, n:{n}, m:{m}, r:{r}")
-print(f"a:{a}")
-print(f"u:{u}, d:{d}")
-print(f"s:{s}, b:{b}")
-print(f"t:{t}, c:{c}")
-print(f"l:{l}")
+# print(f"w:{w}, n:{n}, m:{m}, r:{r}")
+# print(f"a:{a}")
+# print(f"u:{u}, d:{d}")
+# print(f"s:{s}, b:{b}")
+# print(f"t:{t}, c:{c}")
+# print(f"l:{l}")
 
 # Create a length n array to store the state of the generator
 MT = [None] * n
@@ -30,12 +30,13 @@ lower_w_bits = (1 << w) - 1 # 0xffffffff
 lower_mask = (1 << r) - 1 # That is, the binary number of r 1's
 upper_mask = ~lower_mask ^ lower_w_bits
 
-print(f'upper_mask: {bin(upper_mask)}')
+# print(f'upper_mask: {bin(upper_mask)}')
 
 
 # Initialize the generator from a seed
 def seed_mt(seed):
 	global index
+	global MT
 	index = n
 	MT[0] = seed
 
@@ -54,6 +55,8 @@ def seed_mt(seed):
 # calling twist() every n numbers
 
 def extract_number():
+	global index, MT
+
 	if index >= n:
 		if index > n:
 			print(f"index: {index}")
@@ -63,16 +66,42 @@ def extract_number():
 
 	y = MT[index]
 	y = y ^ ((y >> u) & d)
+	y = y ^ ((y << s) & b)
+	y = y ^ ((y << t) & c)
+	y = y ^ (y >> l)
+
+	index = index + 1
+
+	lower_w_bits = (1 << w) - 1 # 0xffffffff
+
+	return y & lower_w_bits
 
 def twist():
+	global index
+	global MT
+
 	for i in range(0, 8):
-		print(bin(MT[i])[0:32])
+		# print(bin(MT[i])[0:32])
 		x = (MT[i] & upper_mask)
-		print(bin(x)[0:32])
-	exit()
+		# print('---------------------')
+		# print(bin(x)[0:32])
+		x = x & (MT[i+1] % n) & lower_mask
+		# print(bin(x)[0:32])
+		# print('=====================')
+		xA = x >> 1
+
+		if (x % 2) != 0: # lowest bit of x is 1
+			xA = xA ^ a
+
+		MT[i] = MT[(i+m) % n] ^ xA
+
+		index = 0
+	#exit()
 
 
 if __name__ == '__main__':
 	
 	seed_mt(0)
-	extract_number()
+	rand1 = extract_number()
+	print(bin(rand1))
+	print(rand1)
