e1a0078be1dd4576769bcfd173ab9f08fe0d1320
Olivier Lasne <lasneolivier@gmail.com>
2023-11-21 20:49:42 +0100

    solved challenge 21

--- set3/set3_21.py
+++ set3/set3_21.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import sys
-
-
-# w, n, m, r, a, u, d, s, b, t, c, l, f
-
+# coefficients for MT19937
 w, n, m, r = 32, 624, 397, 31
 a = 0x9908b0df
 u, d = 11, 0xffffffff
@@ -14,13 +10,6 @@ t, c = 15, 0xefc60000
 l = 18
 f = 1812433253
 
-# print(f"w:{w}, n:{n}, m:{m}, r:{r}")
-# print(f"a:{a}")
-# print(f"u:{u}, d:{d}")
-# print(f"s:{s}, b:{b}")
-# print(f"t:{t}, c:{c}")
-# print(f"l:{l}")
-
 # Create a length n array to store the state of the generator
 MT = [None] * n
 
@@ -28,10 +17,7 @@ index = n+1
 lower_w_bits = (1 << w) - 1 # 0xffffffff
 
 lower_mask = (1 << r) - 1 # That is, the binary number of r 1's
-upper_mask = ~lower_mask ^ lower_w_bits
-
-# print(f'upper_mask: {bin(upper_mask)}')
-
+upper_mask = 0x80000000 # lowest w bits of (not lower_mask)
 
 # Initialize the generator from a seed
 def seed_mt(seed):
@@ -41,15 +27,14 @@ def seed_mt(seed):
 	MT[0] = seed
 
 	for i in range(1, n):
-
-		MT[i] = lower_w_bits ^ (
+		MT[i] = lower_w_bits & (
 			f * (
 				MT[i-1]
 				^ 
-				MT[i-1] >> (w-2)
+				(MT[i-1] >> (w-2))
 				)
 			+ i
-			)
+			) 
 
 # Extract a tempered value based on MT[index]
 # calling twist() every n numbers
@@ -71,37 +56,24 @@ def extract_number():
 	y = y ^ (y >> l)
 
 	index = index + 1
-
-	lower_w_bits = (1 << w) - 1 # 0xffffffff
-
 	return y & lower_w_bits
 
 def twist():
-	global index
-	global MT
+	global index, MT
 
-	for i in range(0, 8):
-		# print(bin(MT[i])[0:32])
-		x = (MT[i] & upper_mask)
-		# print('---------------------')
-		# print(bin(x)[0:32])
-		x = x & (MT[i+1] % n) & lower_mask
-		# print(bin(x)[0:32])
-		# print('=====================')
+	for i in range(0, n):
+		x = (MT[i] & upper_mask) + (MT[(i+1) % n] & lower_mask)
+
 		xA = x >> 1
-
 		if (x % 2) != 0: # lowest bit of x is 1
 			xA = xA ^ a
 
-		MT[i] = MT[(i+m) % n] ^ xA
+		MT[i] = MT[(i + m) % n] ^ xA
 
 		index = 0
-	#exit()
-
 
 if __name__ == '__main__':
 	
 	seed_mt(0)
 	rand1 = extract_number()
-	print(bin(rand1))
 	print(rand1)
