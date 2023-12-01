22f8af39eae4529d946600313f262504590bc34c
Lasne Olivier <lasneolivier@gmail.com>
2023-12-01 20:30:59 +0100

    solved challenge 33 (yeah, we skipped a few)

---
+++ set5/set5_33.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+from hashlib import md5
+import secrets
+
+# Diffie-Hellman constants
+
+class Diffie_Hellman(object):
+	"""docstring for Diffie_Hellman"""
+	
+	# diffie hellman constants
+	default_p = 0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff
+	g = 2
+	
+
+	def __init__(self, p=default_p, g=2):
+		self.p = p
+		self.g = g
+
+		self.secret = secrets.randbelow(pow(2, 1024)-1)
+		self.public = mod_exp(g, self.secret, p)
+		self.shared = None
+
+
+	def derive_secret(self, B):
+		self.shared = mod_exp(B, self.secret, p)
+
+# b puissance e modulo m
+def mod_exp(b, e, m):
+	if m == -1:
+		return 0
+
+	b = b % m
+
+	x = 1
+
+	while e > 0:
+		if e % 2 == 0:
+			x = b * x % m
+
+		b = b * b % m
+		e = e // 2
+
+	return x
+
+# g = 2
+# p = 0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff
+
+
+# a = gen_secret_number()
+# print(f'a: {hex(a)}')
+# A = mod_exp(g, a, p)
+
+# b = 3
+# B = mod_exp(g, b, p)
+
+# print(f'A: {hex(A)}')
+# print(f'B: {hex(B)}')
+
+# # ---------------------
+
+# s1 = mod_exp(A, b, p)
+# print(f's1: {hex(s1)}')
+# print(f"hash: {md5(s1.to_bytes(2048, 'little')).hexdigest()}")
+
+
+if __name__ == '__main__':
+	alphonse = Diffie_Hellman()
+	bettie = Diffie_Hellman()
+
+	print(f"- alphonse -\nsecret: {hex(alphonse.secret)}\npublic: {hex(alphonse.public)}\n")
+	print(f"- bettie -\nsecret: {hex(bettie.secret)}\npublic: {hex(bettie.public)}\n")
